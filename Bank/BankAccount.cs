d11dfd5a8f3f456eb96f866fcbcfe25fde9de35b
Obakeya <eamsradiohe@gmail.com>
2020-05-24 23:03:55 +0900

    テストの改良...テストメソッドが失敗した場合にアサーションを出す

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -11,6 +11,8 @@ namespace BankAccountNS
     {
         private readonly string m_customerName;
         private double m_balance;
+        public const string DebitAmountExceedsBalanceMessage = "引き出し額が残額を超えています。";
+        public const string DebitAmountLessThanZeroMessage = "引き出し額が0より小さいです。";
 
         private BankAccount() { }
 
@@ -31,15 +33,15 @@ namespace BankAccountNS
         }
 
         public void Debit(double amount)
-        {
+        {   //エラーの内容によってエラーメッセージを分岐させる
             if (amount > m_balance)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new ArgumentOutOfRangeException("amount",amount,DebitAmountExceedsBalanceMessage);
             }
 
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
             m_balance -= amount;         }
@@ -59,7 +61,7 @@ namespace BankAccountNS
             BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
 
             ba.Credit(5.77);
-            ba.Debit(11.22);
+            ba.Debit(-10);
             Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
     }
--- BankTests/BankAccountTest.cs
+++ BankTests/BankAccountTest.cs
@@ -6,6 +6,9 @@ namespace BankTests
     [TestClass]
     public class BankTests
     {
+        /// <summary>
+        /// 最初のテストメソッドを作成する
+        /// </summary>
         [TestMethod]
         public void DebitWithValidAmontUpdatesBalance()
         {
@@ -23,5 +26,32 @@ namespace BankTests
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "口座の引き出し額に誤りがあります");
         }
+        /// <summary>
+        /// 単体テストを使用してコードを改良する...スローの確認
+        /// 例外をキャッチしてメッセージを確認する
+        /// </summary>
+        [TestMethod]
+        public void DebitWhenAmountIsLessThanZeroShouldThrowArgumetOutOfRange()
+        {
+            //準備
+            double beginningBalance = 11.99;
+            double debitAmount = 20.00;
+            BankAccount account = new BankAccount("ブライアン　ウォールトン", beginningBalance);
+
+            //実行
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch(System.ArgumentOutOfRangeException e)
+            {
+                //アサート
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("予定していた例外がスローされませんでした。");
+        }
+
     }
 }
